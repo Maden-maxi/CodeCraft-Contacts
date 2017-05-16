28924fdfa01f3ca94f28eff1b95f7b808dd1534a
Denys Dnischenko <maden.maxi@yandex.ru>
2017-05-17 02:54:14 +0300

    Add column to admin contacts table

--- cc-contacts.php
+++ cc-contacts.php
@@ -87,6 +87,7 @@ class CC_Contacts {
         require_once('includes/additional-rest-field.php');
         require_once('includes/class-rest-cc-contacts-controller.php');
         require_once('admin/class-meta-box.php');
+        require_once('admin/class-cc-contacts-table-list.php');
         require_once('public/class-cc-contacts-shortcode-form.php');
     }
 
